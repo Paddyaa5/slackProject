828fac6be9caab31d9d3d40dde6bc2e9e6581d13
jinxbunny <info@jmkit.com>
2020-05-28 14:47:20 +0100

    chat box 'working' and users switchable

--- src/components/MainFeed1.js
+++ src/components/MainFeed1.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import ChatBox from './ChatBox';
-
 import './MainFeed.css';
 import pa from '../img/user1.png';
 import ag from '../img/user2.png';
@@ -13,6 +12,8 @@ import chatright from '../img/textbox_right.png';
 
 class MainFeed1 extends Component{
     state = {
+        username: "Paddy",
+        userpic: pa,
         chats: [
             {pic: rm, user: "Robert Marsh", text: "Good Luck"},
             {pic: ag, user: "Andy Green", text: "So we can ignore the publishing for now?"},
@@ -21,6 +22,40 @@ class MainFeed1 extends Component{
             {pic: rm, user: "Robert Marsh", text: "Another comment"},
         ]
     }
+
+    clickedNew = () => {
+        this.setState({
+          chats: [...this.state.chats,  {pic: this.state.userpic, user: this.state.username, text: this.state.newText}],
+          newText: ""
+        })
+        console.log(this.state.newTask)
+      }
+
+
+      switchUserRM = () => {
+          this.setState({
+              username: "Robert Marsh",
+              userpic: rm
+    })
+    }
+    switchUserAG = () => {
+        this.setState({
+            username: "Andy Green",
+            userpic: ag
+  })
+  }
+  switchUserPA = () => {
+    this.setState({
+        username: "Paddy",
+        userpic: pa
+})
+}
+
+    updatedMessage = event => {
+        this.setState({ newText: event.target.value });
+        console.log(this.state.newText)
+      };
+
     render (){
         let allChats = this.state.chats.map((chat, index) => {
             return <ChatBox key = {index} text = {chat.text} user = {chat.user} pic = {chat.pic}/>
@@ -38,11 +73,12 @@ class MainFeed1 extends Component{
            {allChats}
            </div>
            <div id="inputSection">
-               <input placeholder="Message group3" id="messageIn" autofocus></input>
+               <input placeholder="Message group3" id="messageIn" value={this.state.newMessage} onChange={this.updatedMessage}></input>
                <div id="underInput">
-                   <div id="uiLeft"><img src={chatleft}/></div>
-                   <div id="uiRight"><img src={chatright}/></div>
+                   <div id="uiLeft"><img src={chatleft} alt="text pic"/></div>
+                   <div id="uiRight"><img src={chatright} alt="text pic"/></div>
                </div>
+               <button onClick={this.clickedNew}>Post Message</button> <button onClick={this.switchUserRM}>Rob</button> <button onClick={this.switchUserAG}>Andy</button> <button onClick={this.switchUserPA}>Paddy</button>
            </div>
         </div>
     );
@@ -50,4 +86,4 @@ class MainFeed1 extends Component{
 }
 
 
-export default MainFeed1
+export default MainFeed1;
